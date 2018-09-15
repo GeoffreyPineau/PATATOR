c825ca3c4eadd726708e69ad1b22d753f5851cb9
Pandanym <nympanda@gmail.com>
2018-09-15 15:51:34 +0200

    Grid placement + GameManager

---
+++ Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int columns;
+    public int rows;
+
+    private int[,] squaresArray;
+
+    private void Start()
+    {
+        squaresArray = new int[columns, rows];
+    }
+
+    private void OnDrawGizmos()
+    {
+        squaresArray = new int[columns, rows];
+
+        for (int x = 0; x < squaresArray.GetLength(0); x ++)
+        {
+            for (int y = 0; y < squaresArray.GetLength(1); y++)
+            {
+                Gizmos.DrawSphere(new Vector3(x + .5f, 0f, y + .5f), .3f);
+            }
+        }
+    }
+}
