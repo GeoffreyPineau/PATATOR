fcd43cd2c069b9408265e86baee06d34964f7b5b
Pandanym <nympanda@gmail.com>
2018-09-15 18:42:55 +0200

    Interaction Position

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,13 +15,17 @@ public class PlayerController : MonoBehaviour {
     public float speed = 300;
     public float dashSpeed = 100;
     [Range(0f,1f)]public float turnDamp = 0.1f;
+    [Header("Movement")]
+    public float interactionPositionOffset = .3f;
     [Header("Juice")]
     public Vector3 squashDashValue = new Vector3(0.2f, -0.2f, 0.2f);
     public float squashDashDuration = .8f;
 
+    private bool interactionInput;
     private bool dashInput;
     private Vector3 directionInput;
-    float targetAngle;
+    private float targetAngleFull;
+    private float targetAngle;
     private float currentRotationVelocity;
 
     private Tween squashTween;
@@ -44,20 +48,41 @@ public class PlayerController : MonoBehaviour {
             squashTween = modelPivot.DOPunchScale(squashDashValue, squashDashDuration);
         }
         //
-
-
+        
         //Turn player model
         if (directionInput != Vector3.zero)
         {
             targetAngle = Utilities.Angle(directionInput.x, directionInput.z);
+            targetAngleFull = Utilities.AngleFull(directionInput.x, directionInput.z);
         }
         modelPivot.eulerAngles = new Vector3(0, Mathf.SmoothDampAngle(modelPivot.eulerAngles.y, targetAngle, ref currentRotationVelocity, turnDamp, Mathf.Infinity, Time.deltaTime));
         //
         
+        //Interaction
+        Vector3 direction = new Vector3(Mathf.Cos(targetAngleFull * Mathf.Deg2Rad), 0, Mathf.Sin(targetAngleFull * Mathf.Deg2Rad)).normalized;
+        Vector3 interactionPosition = transform.position + (direction * interactionPositionOffset);
+        interactionPosition = Utilities.GetFlooredPosition(interactionPosition);
+
+        interactionInput = Input.GetMouseButtonDown(1);
+        if (interactionInput)
+        {
+        }
     }
 
     private void FixedUpdate()
     {
         rb.AddForce(directionInput * speed);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Vector3 direction = new Vector3(Mathf.Cos(targetAngleFull * Mathf.Deg2Rad), 0, Mathf.Sin(targetAngleFull * Mathf.Deg2Rad)).normalized;
+        Vector3 interactionPosition = transform.position + (direction * interactionPositionOffset);
+        Gizmos.DrawSphere(interactionPosition, .1f);
+
+        interactionPosition = Utilities.GetFlooredPosition(interactionPosition);
+        Gizmos.color = Color.green;
+        Gizmos.DrawCube(interactionPosition,new Vector3(1,.1f,1));
+        Gizmos.color = Color.gray;
     }
 }
