193d60ea5cfa27b508a591055c1f2cd41091d51f
Geoffrey <g.pineau@rubika-edu.com>
2018-09-16 11:01:46 +0200

    Début Canvas prefab + reparation repo

---
+++ Assets/Scripts/UiRef.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UiRef : MonoBehaviour {
+
+    public RawImage TequilaFuel;
+    public Slider PotatoHeartLife;
+    public TextMeshProUGUI PotatosLife;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
